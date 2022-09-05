efbe7c39fc9bd7e840186ebb31395833a2933a65
Viki <vikamaj88@mail.ru>
2022-09-05 18:48:48 +0300

    task38 done

--- task38/Program.cs
+++ task38/Program.cs
@@ -1,14 +1,10 @@
 ﻿Console.Clear();
 System.Console.WriteLine("Введите количество элементов массива ");
 int [] rand = new int[int.Parse(Console.ReadLine())];
-int sum = 0;
 for (int i = 0; i < rand.Length; i++)
 {
-    rand[i] = new Random().Next(-99, 99);
+    rand[i] = new Random().Next(2, 99);
     System.Console.Write(rand[i]+ " ");
 }
-for (int i = 1; i < rand.Length; i += 2){
-    sum += rand[i];
-}
 System.Console.WriteLine();
-System.Console.WriteLine(sum);
+System.Console.WriteLine("Разница между максимальным и минимальным элементом " + (rand.Max() - rand.Min()));
